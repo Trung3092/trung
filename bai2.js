87fe1e7d39374a2355b09bf78cde0bf34d4f2497
trung <ngothanhtrung3092@gmail.com>
2023-10-07 22:03:44 +0700

    bai2.js

---
+++ bai2.js
@@ -0,0 +1,12 @@
+const commandLineArgs = process.argv.slice(2);
+
+function bai2(n) {
+    let num = +n;
+    let s = 0;
+    for (let i = 12; i <= num * 10 + 2; i += 10) {
+        s += i;
+    }
+    console.log(s);
+}
+
+bai2(...commandLineArgs);

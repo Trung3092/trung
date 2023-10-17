5eb1d4852ad5fe511676af6066a2556f25cfbf18
trung <ngothanhtrung3092@gmail.com>
2023-10-17 14:57:29 +0700

    bai12.js

--- bai11.js
+++ bai11.js
@@ -1,22 +1,31 @@
 const commandLineArgs = process.argv.slice(2);
 
-function primeNumber(n) {
+const primeNumber = function (n) {
     const num = +n;
 
     if (num < 2) {
-        console.log('n không phải là số nguyên tố');
+        console.log(`${num} không phải là số nguyên tố`);
         return;
     }
 
-    for (let i = 2; i < num - 1; i++) {
+    if (num === 2) {
+        console.log(`${num} là số nguyên tố`);
+        return;
+    }
+
+    for (let i = 2; i < num; i++) {
         if (num % i === 0) {
-            console.log('n không phải là số nguyên tố');
+            console.log(`${num} không phải là số nguyên tố`);
             break;
         } else {
-            console.log('n là số nguyên tố');
+            console.log(`${num} là số nguyên tố`);
             break;
         }
     }
-}
+};
 
 primeNumber(...commandLineArgs);
+
+const total = (a, b) => a + b;
+
+module.exports = { total, primeNumber };
---
+++ bai12.js
@@ -0,0 +1,14 @@
+const commandLineArgs = process.argv.slice(2);
+const { total, primeNumber } = require('./bai11');
+
+//console.log('Tổng hai số  :', total(30, 9));
+
+const checkPrimeNumbers = function (n) {
+    for (let i = 1; i <= n; i++) {
+        if (primeNumber(i)) {
+            console.log(i);
+        }
+    }
+};
+
+checkPrimeNumbers(...commandLineArgs);
